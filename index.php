efef605dc8ef5fe0c43cc775b9eb94134cb6ed83
godwinmaga6 <godwinmaga6@gmail.com>
2021-07-18 11:48:51 +0100

    added database connection code to index.php file

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php 
 
+//conect to database
+//NOTE: we can connect using MySQLi or PDO
+//PDO stands for PHP Data Object (You learn this when you become better with MySQLi)
+$conn = mysqli_connect('localhost', 'maga', 'test1234', 'yommi_pizza');
+
+//connection
+if(!$conn){
+    echo 'connection error!' . mysqli_connect_error(); //mysqli_connect_error() lets us know what exactly is wrong with our connection
+}else{
+    echo 'connected';
+}
 
 ?>
 
