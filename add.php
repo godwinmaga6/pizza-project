84aaa2cea63ab7f66456ab41a8214872dcf6b9fe
godwinmaga6 <godwinmaga6@gmail.com>
2021-07-18 08:26:45 +0100

    added checks for actual data using PHP form filters & RegEx

--- add.php
+++ add.php
@@ -7,8 +7,8 @@
 //CHECKS if user is sending data to the server (sends an associative array) OR If empty
 if(isset($_POST['submit'])){ 
     // echo htmlspecialchars($_POST['email']); //this email here is a key, the value of the key is what the user typed on the form
-    echo htmlspecialchars($_POST['title']); //the htmlspecialchars() checks for malicious code and converts it to strings
-    echo htmlspecialchars($_POST['ingredients']);
+    // echo htmlspecialchars($_POST['title']); //the htmlspecialchars() checks for malicious code and converts it to strings
+    // echo htmlspecialchars($_POST['ingredients']);
 
     //SERVER Side form validation
     //check if the form is not empty
@@ -16,19 +16,40 @@ if(isset($_POST['submit'])){
         if(empty($_POST['email'])){
             echo 'An email is required <br/>'; //if empty is echo this
         }{
-            echo htmlspecialchars($_POST['email']); //if not empty, we just echo what is expected in LINE 9
+            // echo htmlspecialchars($_POST['email']); //if not empty, we just echo what is expected in LINE 9
+            // USING php built-in form filter to validate the email
+            $email = $_POST['email']; //grabing the value from the form and storing it in the variable $email
+            // CHECK IF IT'S AN ACTUAL EMAIL using php filter form
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){// THE filter_var() function is built-in to php
+                echo 'email must be a valid email address';
+            }
         }
         //check title
         if(empty($_POST['title'])){
             echo 'A title is required <br/>'; //if empty is echo this
         }{
-            echo htmlspecialchars($_POST['title']); //if not empty, we just echo what is expected in LINE 9
+            // echo htmlspecialchars($_POST['title']); //if not empty, we just echo what is expected in LINE 10
+            // USING RegEx (Regular Expressions) to validate the (Title). 
+            // Cause PHP does to have built-in filter for title etc.
+            $title = $_POST['title'];
+            // CHECKING IF it actually matches our RegEx using php
+            if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $title)){ //first parameter is matching against the second for validation
+                echo 'Ingredients must be a comma separated list';
+            }
+
         }
-        //check email
+        //check ingredients
         if(empty($_POST['ingredients'])){
             echo 'At least one ingredient is required <br/>'; //if empty is echo this
         }{
-            echo htmlspecialchars($_POST['ingredients']); //if not empty, we just echo what is expected in LINE 9
+            // echo htmlspecialchars($_POST['ingredients']); //if not empty, we just echo what is expected in LINE 11
+            // USING RegEx (Regular Expressions) to validate the (comma separated list). 
+            // Cause PHP does to have built-in filter for ingredients etc.
+            $ingredients = $_POST['ingredients'];
+            // CHECKING IF it actually matches our RegEx using php
+            if(!preg_match('/^[a-zA-Z\s]+$/', $ingredients)){ //first parameter is matching against the second for validation
+                echo 'Ingredients must be letters and spaces only';
+            }
         }
 }; // END OF POST CHECK
 
