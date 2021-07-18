a951580449570009f0dc4c5cb7067280b1d59501
godwinmaga6 <godwinmaga6@gmail.com>
2021-07-18 07:35:44 +0100

    added Server Side form validation & if user is sending data to the server code

--- add.php
+++ add.php
@@ -1,5 +1,36 @@
 <?php 
+// THE POST & GET Method
+// The GET method is considered unsecure as it renders user input on the url input field
+// The POST method is considered secure because it does the opposite of the GET method
+// The POST method protects users from cross site scripting
 
+//CHECKS if user is sending data to the server (sends an associative array) OR If empty
+if(isset($_POST['submit'])){ 
+    // echo htmlspecialchars($_POST['email']); //this email here is a key, the value of the key is what the user typed on the form
+    echo htmlspecialchars($_POST['title']); //the htmlspecialchars() checks for malicious code and converts it to strings
+    echo htmlspecialchars($_POST['ingredients']);
+
+    //SERVER Side form validation
+    //check if the form is not empty
+        //check email
+        if(empty($_POST['email'])){
+            echo 'An email is required <br/>'; //if empty is echo this
+        }{
+            echo htmlspecialchars($_POST['email']); //if not empty, we just echo what is expected in LINE 9
+        }
+        //check title
+        if(empty($_POST['title'])){
+            echo 'A title is required <br/>'; //if empty is echo this
+        }{
+            echo htmlspecialchars($_POST['title']); //if not empty, we just echo what is expected in LINE 9
+        }
+        //check email
+        if(empty($_POST['ingredients'])){
+            echo 'At least one ingredient is required <br/>'; //if empty is echo this
+        }{
+            echo htmlspecialchars($_POST['ingredients']); //if not empty, we just echo what is expected in LINE 9
+        }
+}; // END OF POST CHECK
 
 ?>
 
@@ -9,7 +40,7 @@
 
 <section class="container grey-text">
     <h4 class="center">Add a Pizza</h4>
-    <form action="" method="" class="white">
+    <form action="add.php" method="POST" class="white">
         <label>Your Email:</label>
         <input type="text" name="email">
         <label>Your Title:</label>
